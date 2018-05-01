a608b9168c2458e2b93fce67c31e650486b27120
slyngstad60 <kevan.slyngstad@geckodesigns.com>
2018-05-01 15:21:29 -0600

    imporved err message readout

--- src/common/components/EventsFeed.js
+++ src/common/components/EventsFeed.js
@@ -2,26 +2,16 @@ import React from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
-// import {
-// 	loadEvents
-// } from '../actions/Events';
-// import { connect } from 'react-redux';
-
 import LoadingSpinner from './LoadingSpinner';
 import ErrorMessage from './ErrorMessage';
 
 class EventsFeed extends React.PureComponent {
-	// componentWillMount() {
-	// 	this.props.loadEvents();
-	// }
 
 	render() {
 		console.log(this.props);
 		const { events, loading, error } = this.props.data;
 
-		if (!events) {
-			return false;
-		}
+
 		let eventsContent = null;
 
 		if (loading) {
@@ -29,7 +19,7 @@ class EventsFeed extends React.PureComponent {
 		}
 
 		if (error) {
-			return <ErrorMessage message={error} />
+			return <ErrorMessage message={error.message} />
 		}
 
 		eventsContent = events.map((event, index) => {
@@ -50,18 +40,6 @@ class EventsFeed extends React.PureComponent {
 	}
 };
 
-
-
-// const mapStateToProps = state => {
-// 	return {
-// 		events: state.events.events,
-// 		isFetching: state.events.isFetching,
-// 		failed: state.events.failed
-// 	}
-// }
-
-// export default connect(mapStateToProps, { loadEvents })(EventsFeed);
-// export default EventsFeed;
 export default graphql(gql`
 	query getAllEvents {
 		events{
--- src/server/controllers/eventController.js
+++ src/server/controllers/eventController.js
@@ -5,10 +5,10 @@ const Event = mongoose.model('Event');
 
 module.exports = {
 	getEvents: async (req, res) => {
-		const events = await Event.find({}).limit(10);
-
+		// const events = await Event.find({}).limit(10);
+		const events = null;
 		if (!events) {
-			// res.send(400);
+			throw new Error('There was an error getting the events...');
 
 			return;
 		}
